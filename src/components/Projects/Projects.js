414ad0461b52ede80eac66b9c04b725deacdc0bc
Jay Patel <76567906+jaypatel9800@users.noreply.github.com>
2021-08-23 20:26:07 +0530

    Update project page move all components to separate file.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./projects.css";
-import HighlightOffRoundedIcon from "@material-ui/icons/HighlightOffRounded";
 import { useHistory } from "react-router-dom";
 import Back from "../../assets/back.svg";
 import ChatApp from "../../assets/project/chatApp.png";
@@ -9,6 +8,8 @@ import todo from "../../assets/project/todo.jpg";
 import amazon from "../../assets/project/amazon.png";
 import sunShine from "../../assets/project/sunshine.jpg";
 import form from "../../assets/project/stepform.png";
+import ProjectComp from "./projectComp";
+import InnerComp from "./InnerComp";
 
 const Projects = () => {
   const [first, setFirst] = useState(true);
@@ -32,238 +33,133 @@ const Projects = () => {
         <div className="project__items">
           <div className="project__project">
             {first && (
-              <div className="project__img">
-                <a
-                  href="https://csb-tnopx.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={ChatApp} alt="chat app" />
-                </a>
-                <button
-                  className="project__detailBtn"
-                  onClick={() => setFirst(false)}
-                >
-                  Details
-                </button>
-              </div>
+              <ProjectComp
+                link="https://csb-tnopx.netlify.app/"
+                MainImg={ChatApp}
+                altName="chat app"
+                onclick={() => setFirst(false)}
+              />
             )}
             {!first && (
-              <div
-                onMouseLeave={() => setFirst(true)}
-                className="project__info"
-              >
-                <h2>Real Time ChatApp</h2>
-                <div>
-                  <p>ReactJs</p>
-                  <p>Socket.io</p>
-                  <p>Nodejs</p>
-                  <p>React Emoji</p>
-                  <p>Material-Ui</p>
-                </div>
-                <button
-                  className="project__closeBtn"
-                  onClick={() => setFirst(true)}
-                >
-                  <HighlightOffRoundedIcon />
-                </button>
-              </div>
+              <InnerComp
+                leave={() => setFirst(true)}
+                title="Real Time ChatApp"
+                one="ReactJs"
+                two="Socket.io"
+                three="Nodejs"
+                four="Material-Ui"
+                closeBtn = {() => setFirst(true)}
+              />
             )}
           </div>
 
           <div className="project__project">
             {second && (
-              <div className="project__img">
-                <a
-                  href="https://csb-c0snf.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={ipAdd} alt="ip address" />
-                </a>
-                <button
-                  className="project__detailBtn"
-                  onClick={() => setSecond(false)}
-                >
-                  Details
-                </button>
-              </div>
+              <ProjectComp
+                link="https://csb-c0snf.netlify.app/"
+                MainImg={ipAdd}
+                altName="ip Address"
+                onclick={() => setSecond(false)}
+              />
             )}
             {!second && (
-              <div
-                onMouseLeave={() => setSecond(true)}
-                className="project__info"
-              >
-                <h2>Ip Address Tracker</h2>
-                <div>
-                  <p>ReactJs</p>
-                  <p>IP Geolocation API</p>
-                  <p>leafletjs</p>
-                  <p>styled components</p>
-                </div>
-                <button
-                  className="project__closeBtn"
-                  onClick={() => setSecond(true)}
-                >
-                  <HighlightOffRoundedIcon />
-                </button>
-              </div>
+              <InnerComp
+              leave={() => setSecond(true)}
+              title="Ip Address Tracker"
+              one="ReactJs"
+              two="IP Geolocation API"
+              three="leafletjs"
+              four="styled components"
+              closeBtn = {() => setSecond(true)}
+            />   
             )}
           </div>
 
           <div className="project__project">
             {third && (
-              <div className="project__img">
-                <a
-                  href="https://jaypatel9800.github.io/todos/#/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={todo} alt="todo" />
-                </a>
-                <button
-                  className="project__detailBtn"
-                  onClick={() => setThird(false)}
-                >
-                  Details
-                </button>
-              </div>
+              <ProjectComp
+                link="https://jaypatel9800.github.io/todos/#/"
+                MainImg={todo}
+                altName="todo"
+                onclick={() => setThird(false)}
+              />
             )}
             {!third && (
-              <div
-                onMouseLeave={() => setThird(true)}
-                className="project__info"
-              >
-                <h2>Todo App</h2>
-                <div>
-                  <p>ReactJS</p>
-                  <p>css</p>
-                  <p>React Routers</p>
-                  <p> With Theme Toggle</p>
-                </div>
-                <button
-                  className="project__closeBtn"
-                  onClick={() => setThird(true)}
-                >
-                  <HighlightOffRoundedIcon />
-                </button>
-              </div>
+              <InnerComp
+              leave={() => setThird(true)}
+              title="Todo App"
+              one="ReactJs"
+              two="CSS"
+              three="React Routers"
+              four="With Theme Toggle"
+              closeBtn = {() => setThird(true)}
+            />
             )}
           </div>
 
           <div className="project__project">
             {fourth && (
-              <div className="project__img">
-                <a
-                  href="https://qkd45.csb.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={amazon} alt="amazon" />
-                </a>
-                <button
-                  className="project__detailBtn"
-                  onClick={() => setFourth(false)}
-                >
-                  Details
-                </button>
-              </div>
+              <ProjectComp
+                link="https://qkd45.csb.app/"
+                MainImg={amazon}
+                altName="amazon"
+                onclick={() => setFourth(false)}
+              />
             )}
             {!fourth && (
-              <div
-                onMouseLeave={() => setFourth(true)}
-                className="project__info"
-              >
-                <h2>Amazon Clone</h2>
-                <div>
-                  <p>ReactJs</p>
-                  <p>Context API</p>
-                  <p>Firebase Auth</p>
-                  <p>React Routers</p>
-                </div>
-                <button
-                  className="project__closeBtn"
-                  onClick={() => setFourth(true)}
-                >
-                  <HighlightOffRoundedIcon />
-                </button>
-              </div>
+              <InnerComp
+              leave={() => setFourth(true)}
+              title="Amazon Clone"
+              one="ReactJs"
+              two="Context API"
+              three="Firebase Auth"
+              four="React Routers"
+              closeBtn = {() => setFourth(true)}
+            />
             )}
           </div>
 
           <div className="project__project">
             {fifth && (
-              <div className="project__img">
-                <a
-                  href="https://jaypatel9800.github.io/sunShine-landingPage/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={sunShine} alt="sunShine landing page" />
-                </a>
-                <button
-                  className="project__detailBtn"
-                  onClick={() => setFifth(false)}
-                >
-                  Details
-                </button>
-              </div>
+              <ProjectComp
+                link="https://jaypatel9800.github.io/sunShine-landingPage/"
+                MainImg={sunShine}
+                altName="sunShine landing page"
+                onclick={() => setFifth(false)}
+              />
             )}
             {!fifth && (
-              <div
-                onMouseLeave={() => setFifth(true)}
-                className="project__info"
-              >
-                <h2>SunShine Landing Page</h2>
-                <div>
-                  <p>HTML</p>
-                  <p>CSS Grid</p>
-                  <p>JavaScript</p>
-                </div>
-                <button
-                  className="project__closeBtn"
-                  onClick={() => setFifth(true)}
-                >
-                  <HighlightOffRoundedIcon />
-                </button>
-              </div>
+              <InnerComp
+              leave={() => setFifth(true)}
+              title="SunShine Landing Page"
+              one="HTML"
+              two="CSS Flex"
+              three="CSS Grid"
+              four="JavaScript"
+              closeBtn = {() => setFifth(true)}
+            />
             )}
           </div>
 
           <div className="project__project">
             {six && (
-              <div className="project__img">
-                <a
-                  href="https://csb-qc7y4.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={form} alt=" React step form" />
-                </a>
-                <button
-                  className="project__detailBtn"
-                  onClick={() => setSix(false)}
-                >
-                  Details
-                </button>
-              </div>
+              <ProjectComp
+                link="https://csb-qc7y4.netlify.app/"
+                MainImg={form}
+                altName="React step form"
+                onclick={() => setSix(false)}
+              />
             )}
             {!six && (
-              <div onMouseLeave={() => setSix(true)} className="project__info">
-                <h2>React Step Form</h2>
-                <div>
-                  <p>ReactJs</p>
-                  <p>Material-Ui</p>
-                  <p>React hooks helper</p>
-                  <p>With validation</p>
-                </div>
-                <button
-                  className="project__closeBtn"
-                  onClick={() => setSix(true)}
-                >
-                  <HighlightOffRoundedIcon />
-                </button>
-              </div>
+              <InnerComp
+              leave={() => setSix(true)}
+              title="React Step Form"
+              one="ReactJs"
+              two="Material-Ui"
+              three="React hooks helper"
+              four="With validation"
+              closeBtn = {() => setSix(true)}
+            />
             )}
           </div>
         </div>
